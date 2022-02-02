fabe59369d4b52da8fe035ba565523d3b5adc38f
ChristopherEeles <christopher.eeles@outlook.com>
2022-02-02 18:22:03 -0500

    Update: AKS cluster provisioning script done

--- scripts/cluster_setup.sh
+++ scripts/cluster_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+## -- Authenticate with Azure (requires manual user input)
+az login
+
 # -- Read in configuration files
 source .azure
+## TODO:: Make configuration file for cluster specifications
+# source .aks
 
-# -- Configure which Azure resources to use
+# -- Create a Blob storage container
 
 # Get the first key for the specified storage account
 stgkey=$(
@@ -15,15 +20,46 @@ stgkey=$(
 
 containername=snakemake-test
 
-# -- Create a Blob storage container
+# Create the container, fails if it exists
 az storage container create \
     --resource-group $resgroup \
     --account-name $stgacct \
     --account-key $stgkey \
     --name $containername
 
-# -- Configure AKS cluster
+# -- Upload data to blob container
+
+# Create tmp directory, if it doesn't exist
+[ ! -d tmp ] && mkdir tmp
+
+# Fetch the data
+git clone \
+    https://github.com/snakemake/snakemake-tutorial-data.git \
+    tmp/snakemake-tutorial-data
+
+# Upload to blob storage
+az storage blob upload-batch \
+    -d $containername \
+    --account-name $stgacct \
+    --account-key $stgkey \
+    -s tmp/snakemake-tutorial-data/data/ \
+    --destination-path data
+
+# -- Configure and create AKS cluster
+
 clustername=aks-snakemake
 
+az aks create \
+    --resource-group $resgroup \
+    --name $clustername \
+    --vm-set-type VirtualMachineScaleSets \
+    --load-balancer-sku standard \
+    --enable-cluster-autoscaler \
+    --node-count 1 \
+    --min-count 1 \
+    --max-count 3 \
+    --node-vm-size Standard_D3_v2
 
-# -- Create AKS cluster
+az aks get-credentials \
+    --resource-group $resgroup \
+    --name $clustername
